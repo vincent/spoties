461602dd0ffba97c16e58acf66041d3b206981c2
Vincent Lark <v.lark@astonai.com>
2025-04-26 19:06:17 +0200

    feat(event form) more UX improvements

--- sk/src/lib/components/Admin/Events/EventForm.svelte
+++ sk/src/lib/components/Admin/Events/EventForm.svelte
@@ -3,7 +3,7 @@
   import LocationsSelector from "../Locations/LocationsSelector.svelte";
   import { AdminEventStore } from "$lib/stores/admin-event-form.svelte";
   import FieldErrors from "$lib/components/Shared/FieldErrors.svelte";
-  import { Label, FloatingLabelInput, A } from "flowbite-svelte";
+  import { Label, FloatingLabelInput, A, Accordion, AccordionItem } from "flowbite-svelte";
   import QuestionsForm from "../Questions/QuestionsForm.svelte";
   import RichText from "$lib/components/Shared/RichText.svelte";
   import { t } from "$lib/i18n";
@@ -14,45 +14,49 @@
 
 <form class="flex justify-center items-start" onsubmit={() => AdminEventStore.updateEvent($AdminEventStore)}>
   <div class="w-3/4">
-    <div class="mb-6 p-3">
-      <div class="mb-6">
-        <FloatingLabelInput required classDiv="mb-4" classInput="text-3xl" color={validation?.error?.fieldErrors?.title ? "red" : undefined} type="text" bind:value={$AdminEventStore.title}>
-          {$t('event.form.event_title')}
-        </FloatingLabelInput>
-        <FieldErrors validationErrors={validation?.error?.fieldErrors?.title} />
-      </div>
-
-      {#if $AdminEventStore.id}
-        <div class="mb-6">
-          <Label class="mb-2 block text-2xl">{$t('event.form.public_link')}</Label>
-          <A target="_blank" href={`/event/${$AdminEventStore.id}`}>{`/event/${$AdminEventStore.id}`}</A>
-        </div>
-      {/if}
-
-      <div class="mb-6">
-        <Label for="event-description" class="mb-2 block text-2xl">{$t('event.form.description')}</Label>
-        <RichText bind:value={$AdminEventStore.description} color={validation?.error?.fieldErrors?.description ? "red" : undefined}/>
-        <FieldErrors validationErrors={validation?.error?.fieldErrors?.description} />
-      </div>
+    <div class="mb-6">
+      <FloatingLabelInput required classDiv="mb-4" classInput="text-3xl" color={validation?.error?.fieldErrors?.title ? "red" : undefined} type="text" bind:value={$AdminEventStore.title}>
+        {$t('event.form.event_title')}
+      </FloatingLabelInput>
+      <FieldErrors validationErrors={validation?.error?.fieldErrors?.title} />
     </div>
 
-    <div class="mb-6 p-3">
-      <Label for="event-questions" class="mb-2 block text-2xl">{$t('event.form.questions')}</Label>
-      <QuestionsForm
-        bind:value={$AdminEventStore.questions}
-      />
-    </div>
+    {#if $AdminEventStore.id}
+      <div class="mb-6">
+        <Label class="mb-2 block text-2xl">{$t('event.form.public_link')}</Label>
+        <A target="_blank" href={`/event/${$AdminEventStore.id}`}>{`/event/${$AdminEventStore.id}`}</A>
+      </div>
+    {/if}
 
-    <div class="mb-6 p-3">
-      <Label for="event-locations" class="mb-2 block text-2xl">{$t('event.form.locations')}</Label>
-      <LocationsSelector
-        {locations}
-        bind:value={$AdminEventStore.locations}
-        submit={() => AdminEventStore.updateEvent($AdminEventStore)}
-      />
+    <div class="mb-6 accordion">
+      <Accordion>
+        <AccordionItem>
+          <span slot="header">{$t('event.form.description')}</span>
+          <RichText bind:value={$AdminEventStore.description} color={validation?.error?.fieldErrors?.description ? "red" : undefined}/>
+          <FieldErrors validationErrors={validation?.error?.fieldErrors?.description} />
+        </AccordionItem>
+        <AccordionItem>
+          <span slot="header">{$t('event.form.questions')}</span>
+          <QuestionsForm bind:value={$AdminEventStore.questions} />
+        </AccordionItem>
+        <AccordionItem>
+          <span slot="header">{$t('event.form.locations')}</span>
+          <LocationsSelector
+            {locations}
+            bind:value={$AdminEventStore.locations}
+            submit={() => AdminEventStore.updateEvent($AdminEventStore)}
+          />
+        </AccordionItem>
+      </Accordion>
     </div>
   </div>
   <div class="ml-3 w-1/4 sticky top-18">
     <SelectedLocationsSummary submit={() => AdminEventStore.updateEvent($AdminEventStore)} />
   </div>
 </form>
+
+<style>
+  :global(.accordion .group:first-child + div div.p-5) {
+    padding: 0 !important;
+  }
+</style>
